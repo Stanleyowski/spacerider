90deaf630b4296dc695ac5396a4c25e18de7f13d
kris <kris@krej.net>
2020-01-25 11:25:36 +0100

    Zbieranie danych z grupy testów

--- Assets/DataAnalyst.cs
+++ Assets/DataAnalyst.cs
@@ -4,16 +4,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataAnalyst : MonoBehaviour {
+    static DataAnalyst instance;
+    [Multiline] public string log = "";
+
+    int amountOfTests = 10;
+    int currentTestNr = 0;
+    float scoresSum = 0;
 
     void Awake() {
-        DontDestroyOnLoad(gameObject);
-        
-        Time.timeScale = 1;
+        if (instance == null) 
+            Init();
+        else 
+            Destroy(gameObject);
+    }
+
+    void Init() {
+        Debug.Log("initializing data analyst");
+        Time.timeScale = 10;
         GameManager.OnGameEndedEvent = OnGameEndedEventHandler;
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     void OnGameEndedEventHandler() {
         var scoreManager = FindObjectOfType<ScoreManager>();
-        Debug.Log($"{scoreManager.scoreCounter:0}");
+        scoresSum += scoreManager.scoreCounter;
+        currentTestNr++;
+        if (currentTestNr == amountOfTests)
+            FinshTestingSeries();
+    }
+
+    void FinshTestingSeries() {
+        Log($"Średni czas: {(scoresSum / amountOfTests):0}");
+        currentTestNr = 0;
+        scoresSum = 0;
+    }
+
+    void Log(string txt) {
+        Debug.Log(txt + "\n");
+        log += txt + "\n";
     }
 }
