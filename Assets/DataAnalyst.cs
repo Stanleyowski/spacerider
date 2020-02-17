e95d2b74c540440c8c4bf7b62ed9920dc9593055
Stanleyowski <dawid2815@interia.eu>
2020-02-17 20:53:35 +0100

    Revert "Zbieranie danych z grupy testów"
    This reverts commit 90deaf630b4296dc695ac5396a4c25e18de7f13d.

--- Assets/DataAnalyst.cs
+++ Assets/DataAnalyst.cs
@@ -4,44 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataAnalyst : MonoBehaviour {
-    static DataAnalyst instance;
-    [Multiline] public string log = "";
-
-    int amountOfTests = 10;
-    int currentTestNr = 0;
-    float scoresSum = 0;
 
     void Awake() {
-        if (instance == null) 
-            Init();
-        else 
-            Destroy(gameObject);
-    }
-
-    void Init() {
-        Debug.Log("initializing data analyst");
-        Time.timeScale = 10;
-        GameManager.OnGameEndedEvent = OnGameEndedEventHandler;
-        instance = this;
         DontDestroyOnLoad(gameObject);
+        
+        Time.timeScale = 1;
+        GameManager.OnGameEndedEvent = OnGameEndedEventHandler;
     }
 
     void OnGameEndedEventHandler() {
         var scoreManager = FindObjectOfType<ScoreManager>();
-        scoresSum += scoreManager.scoreCounter;
-        currentTestNr++;
-        if (currentTestNr == amountOfTests)
-            FinshTestingSeries();
-    }
-
-    void FinshTestingSeries() {
-        Log($"Średni czas: {(scoresSum / amountOfTests):0}");
-        currentTestNr = 0;
-        scoresSum = 0;
-    }
-
-    void Log(string txt) {
-        Debug.Log(txt + "\n");
-        log += txt + "\n";
+        Debug.Log($"{scoreManager.scoreCounter:0}");
     }
 }
